d45a8a39eac5983605c491f7209fb346516acf53
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-09-10 09:22:17 +0100

    Added user_id column back to reviews

--- BetterThanTelly/db/schema.rb
+++ BetterThanTelly/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_09_113503) do
+ActiveRecord::Schema.define(version: 2019_09_10_081758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2019_09_09_113503) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "event_id", null: false
+    t.integer "user_id"
     t.index ["event_id"], name: "index_reviews_on_event_id"
   end
 
