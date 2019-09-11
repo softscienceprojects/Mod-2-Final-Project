a4d484fbf36a9ac0d8b116d860344732ce934c69
99chris99 <chrishodge87@gmail.com>
2019-09-11 10:32:53 +0100

    updated User reccomend methods

--- BetterThanTelly/app/models/user.rb
+++ BetterThanTelly/app/models/user.rb
@@ -32,28 +32,96 @@ class User < ApplicationRecord
         get_host_events.select{|event| event.event_date >= Date.today}
     end
 
+    #Reccomendation Methods...
+
+
+    def get_positive_reviews
+        all_reviews = Review.where(user_id: self.id)
+        all_reviews.select{|review|review.rating > Review.rating_range.max/2 }
+        return all_reviews
+    end
+
+
+
+    #Category Based
     def get_favourite_cat_by_attendence
-       all_cats = get_events.map{|event|event.category_id}
-       if all_cats != []
-       favourite_cat = all_cats.max_by{|cat|all_cats.count(cat)}
-       Category.find(favourite_cat).name
-       else
+        if get_events != []
+            cat_hash  = Hash.new(0)
+            get_events.each do |event|    
+                cat_hash[event.category_id] += 1
+            end
+            cat_hash.sort_by{|category,total_rating| -total_rating}.first 3
+        else
         0
-       end
+        end
     end
 
     def get_favourite_cat_by_reviews
-        all_reviews = Review.where(user_id: self.id)
-            if all_reviews != []
-                cat_hash  = Hash.new(0)
-                all_reviews.each do |review|
-                cat_hash[review.event.category.name] += review.rating
-                end
-                cat_hash.sort_by {|category,total_rating|total_rating}.last[0]
-            else
-            0
+        if get_positive_reviews != []
+            cat_hash  = Hash.new(0)
+            get_positive_reviews.each do |review|    
+                cat_hash[review.event.category_id] += review.rating
             end
+            cat_hash.sort_by{|category,total_rating| -total_rating}.first 3
+        else
+        0
+        end
     end
+
+    #Host_based
+
+    def get_favourite_host_by_reviews
+        if get_positive_reviews != []
+            host_hash = Hash.new(0)
+            get_positive_reviews.each do |review|
+                host_hash[review.event.user_id] += review.rating
+            end
+            host_hash.sort_by {|host, total_rating| -total_rating}
+        else
+            0
+        end
+    end
+
+    def get_favourite_host_by_attendence
+        if get_events != []
+            host_hash  = Hash.new(0)
+            get_events.each do |event|    
+                host_hash[event.user_id] += 1
+            end
+            host_hash.sort_by{|host,total_rating| -total_rating}
+        else
+        0
+        end
+    end
+
+    #Venue Based
+
+    def get_favourite_venue_by_reviews
+        if get_positive_reviews != []
+            venue_hash = Hash.new(0)
+            get_positive_reviews.each do |review|
+                venue_hash[review.event.venue_id] += review.rating
+            end
+            venue_hash.sort_by {|venue, total_rating| -total_rating}
+        else
+            0
+        end
+    end
+
+    def get_favourite_host_by_attendence
+        if get_events != []
+            host_hash  = Hash.new(0)
+            get_events.each do |event|    
+                host_hash[event.user_id] += 1
+            end
+            host_hash.sort_by{|host,total_rating| -total_rating}
+        else
+        0
+        end
+    end
+
+
+
 
 
     
