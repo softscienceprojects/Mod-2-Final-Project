d6d65742bee1de758380570d135e410a007031b1
99chris99 <chrishodge87@gmail.com>
2019-09-11 18:13:28 +0100

    added event filter

--- BetterThanTelly/app/models/event.rb
+++ BetterThanTelly/app/models/event.rb
@@ -42,14 +42,21 @@ class Event < ApplicationRecord
     self.event_in_future? && self.event_attendee?
   end
 
+
   def self.category_filter(search_term)
-    
-    if search_term != nil
+
+    if search_term.present?
       category = Category.find_by(name: search_term["name"])
+      if category.present?
         Event.all_future_events_in_date_order.select{|event|event.category_id == category.id}  
+      else
+        Event.all_future_events_in_date_order
+      end
+
     else
       Event.all_future_events_in_date_order
     end
+  
   end
 
   
